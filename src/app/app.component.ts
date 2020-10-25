caa1bd614dda81d30296ed65e15af349773859b5
m-nakamine <kawarabasami.dev@gmail.com>
2020-10-26 00:54:15 +0900

    #1 モック作成
    チェックボックスのON/OFFでチャートへグラフ追加/削除を制御

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 import { Subscription } from 'rxjs';
 import { Prefecture } from './entity/prefecture';
 import { ReasasApiService } from './reasas-api.service';
@@ -9,9 +10,53 @@ import { ReasasApiService } from './reasas-api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reasas-api-sample';
 
+  /**
+   * 折れ線グラフ表示用.
+   *
+   * @type {Chart}
+   * @memberof AppComponent
+   */
+  chart: Chart = new Chart({
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: '',
+    },
+    credits: {
+      enabled: false
+    },
+    xAxis: {
+      title: { text: '年度' },
+
+    },
+    yAxis: {
+      title: { text: '総人口' }
+    },
+    legend: {
+      align: 'right',
+      layout: 'vertical',
+      verticalAlign: 'top',
+    },
+    series: [
+    ]
+  });
+
+  /**
+   * 都道府県一覧データ.
+   *
+   * @type {Prefecture[]}
+   * @memberof AppComponent
+   */
   prefectures: Prefecture[] = [];
+
+  /**
+   * 都道府県一覧 インデックス情報管理用.
+   *
+   * @memberof AppComponent
+   */
+  addedChartPrefs: Prefecture[] = [];
 
   /**
    * イベント購読解除用.
@@ -38,4 +83,34 @@ export class AppComponent {
     // イベント購読を解除
     this._subsc.unsubscribe();
   }
+
+  /**
+   *  チェックボックスON/OFF切替時の処理.
+   *  チャートへのグラフ追加/削除を行う.
+   * 
+   * @param isChecked 
+   * @param pref 
+   */
+  onCheckboxChanged(isChecked: boolean, pref: Prefecture) {
+
+    if (isChecked) {
+      this.addedChartPrefs.push(pref);
+      this.chart.addSeries({
+        name: `${pref.prefName}`,
+        type: 'line',
+        data: [
+          [1990, Math.floor(Math.random() * 10)],
+          [2000, Math.floor(Math.random() * 10)],
+          [2010, Math.floor(Math.random() * 10)]
+        ]
+      }, true, true);
+    } else {
+      const idx = this.addedChartPrefs.findIndex(item => item.prefCode === pref.prefCode);
+      if (idx != null) {
+        this.chart.removeSeries(idx);
+        this.addedChartPrefs = this.addedChartPrefs.filter(item => item.prefCode !== pref.prefCode);
+      }
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartModule } from 'angular-highcharts';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     NoopAnimationsModule,
     HttpClientModule,
+    MatCheckboxModule,
+    ChartModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
